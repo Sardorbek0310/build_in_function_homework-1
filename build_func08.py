591d8be218bb91885ca363b009795fd8af592dfd
sardorbek0310 <alimkulov03sardorbek@gmail.com>
2025-07-02 06:16:16 -0400

    8 done

--- build_func08.py
+++ build_func08.py
@@ -1,3 +1,5 @@
+x=7
+y=1
 def main(x, y):
     """Integer type variables 'x' and 'y' are given. Return the value of the expression in README.md file.
     https://github.com/codeschool43/Build_in_function_homework#build_func08
@@ -9,4 +11,5 @@ def main(x, y):
     Returns:
         int: the value of the expression
     """
-    return
+    return 5*pow(x,2)*pow(y,3)+x*pow(y,2)
+print(main(x, y))
